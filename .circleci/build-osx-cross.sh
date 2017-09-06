7e9eb2bee14991655a8599756c9667865c2da639
M. Adam Kendall <michael.kendall@gsa.gov>
2017-09-06 09:48:37 -0400

    Address review comments

--- .circleci/build-osx-cross.sh
+++ .circleci/build-osx-cross.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -e
+
 cd /tmp
 git clone https://github.com/tpoechtrager/osxcross.git
 cd osxcross
-curl -#L https://github.com/phracker/MacOSX-SDKs/releases/download/10.13/MacOSX10.11.sdk.tar.xz -o tarballs/MacOSX10.11.sdk.tar.xz
+curl --progress-bar -L https://github.com/phracker/MacOSX-SDKs/releases/download/10.13/MacOSX10.11.sdk.tar.xz -o tarballs/MacOSX10.11.sdk.tar.xz
 UNATTENDED=1 OSX_VERSION_MIN=10.8 ./build.sh
